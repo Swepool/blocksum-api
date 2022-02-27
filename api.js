92ba26085dced74bbd8e8585d1041696bc509adf
Swepool <36674091+Swepool@users.noreply.github.com>
2022-02-27 03:16:19 +0100

    cors..

--- api.js
+++ api.js
@@ -2,14 +2,12 @@ const app = require('express')();
 const cors = require("cors");
 const PORT = 8081
 
-//Set CORS
-app.use(cors({
-    methods: ['GET']
-}));
 
 //Get routes
 const nodeRoute = require('./routes/get-nodes')
-app.use('/nodes', nodeRoute)
+app.use('/nodes', nodeRoute, cors({
+    methods: ['GET']
+}))
 
 //Start message
 app.listen(
