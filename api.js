49e3a0a8fa4d5d87b43b89b476997f14ae583a10
Swepool <36674091+Swepool@users.noreply.github.com>
2022-02-27 03:14:26 +0100

    Update api.js

--- api.js
+++ api.js
@@ -2,6 +2,12 @@ const app = require('express')();
 const cors = require("cors");
 const PORT = 8081
 
+//Set CORS
+app.use(cors({
+    methods: ['GET']
+}));
+
+//Get routes
 const nodeRoute = require('./routes/get-nodes')
 app.use('/nodes', nodeRoute)
 
