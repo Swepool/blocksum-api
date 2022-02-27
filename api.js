840e542cc19aa43296eab0f968e75e4e22d900f0
Swepool <36674091+Swepool@users.noreply.github.com>
2022-02-27 03:07:00 +0100

    added routes

--- api.js
+++ api.js
@@ -1,60 +1,17 @@
 const app = require('express')();
-const fetch = require('cross-fetch')
-const cors = require('cors');
-const PORT = 8080
+const cors = require("cors");
+const PORT = 8081
+
+const nodeRoute = require('./routes/get-nodes')
+app.use('/nodes', nodeRoute)
+
+//Set CORS
+app.use(cors({
+    methods: ['GET']
+}));
 
 //Start message
 app.listen(
     PORT,
     () => console.log(`It's live on http://localhost:${PORT} 🥳`)
 )
-
-app.use(cors({
-    methods: ['GET']
-}));
-
-let nodes = []
-
-//Fetch list from github and iterate over all nodes
-function getNodeData() {
-    console.log("🚨 Getting nodes")
-    nodes = []
-    fetch(`https://raw.githubusercontent.com/kryptokrona/kryptokrona-nodes-list/master/nodes.json`)
-        .then(res => res.json())
-        .then(data => {
-            for (const node of data.nodes){
-                fetch('http://' + node.url + ':11898/getinfo')
-                    .then(res => res.json())
-                    .then(data => {
-                        createNodeList(node, data)
-                    })
-                    .catch(err => console.log(err))
-            }
-        })
-        .catch(err => console.log(err))
-}
-
-//Create list of nodes
-function createNodeList(node, data) {
-    nodes.push({
-        nodeName: node.name,
-        nodeUrl:node.url,
-        nodePort: node.port,
-        nodeFee: node.fee,
-        nodeSsl: node.ssl,
-        nodeHeight: data.height,
-        connectionsIn: data.incoming_connections_count,
-        connectionsOut: data.outgoing_connections_count,
-        nodeSynced: data.synced,
-        nodeStatus: data.status,
-        nodeVersion: data.version
-    })
-}
-
-setInterval(getNodeData, 90000)
-
-
-//Listen for /nodes
-app.get('/nodes', (req, res) => {
-    res.status(200).send({nodes})
-})
---
+++ routes/get-nodes.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const router = require('express').Router();
+
+const fetch = require("cross-fetch");
+
+let nodes = []
+
+//Fetch list from github and iterate over all nodes
+    function getNodeData() {
+        console.log("🚨 Getting nodes")
+        nodes = []
+        fetch(`https://raw.githubusercontent.com/kryptokrona/kryptokrona-nodes-list/master/nodes.json`)
+            .then(res => res.json())
+            .then(data => {
+                for (const node of data.nodes){
+                    fetch('http://' + node.url + ':11898/getinfo')
+                        .then(res => res.json())
+                        .then(data => {
+                            createNodeList(node, data)
+                        })
+                        .catch(err => console.log(err))
+                }
+            })
+            .catch(err => console.log(err))
+    }
+
+//Create list of nodes
+    function createNodeList(node, data) {
+        nodes.push({
+            nodeName: node.name,
+            nodeUrl:node.url,
+            nodePort: node.port,
+            nodeFee: node.fee,
+            nodeSsl: node.ssl,
+            nodeHeight: data.height,
+            connectionsIn: data.incoming_connections_count,
+            connectionsOut: data.outgoing_connections_count,
+            nodeSynced: data.synced,
+            nodeStatus: data.status,
+            nodeVersion: data.version
+        })
+    }
+
+setInterval(getNodeData, 5000)
+
+//Listen for /nodes
+router.get('/', (req, res) => {
+    res.status(200).send({nodes})
+})
+
+module.exports = router
