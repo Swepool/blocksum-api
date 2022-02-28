1f8f1630eb185a08ac15ddce33a43a6e58fe763a
Swepool <36674091+Swepool@users.noreply.github.com>
2022-03-01 00:22:34 +0100

    Seperate

--- api.js
+++ api.js
@@ -18,41 +18,6 @@ app.use('/pools', cors(corsOptions), poolsRoute)
 const supplyRoute = require('./routes/get-supply')
 app.use('/supply', cors(corsOptions), supplyRoute)
 
-//NBX
-const spreadNbxRoute = require('./routes/nbx/nbx-spread')
-app.use('/nbx/spread', cors(corsOptions), spreadNbxRoute)
-
-const priceNbxRoute = require('./routes/nbx/nbx-price')
-app.use('/nbx/price', cors(corsOptions), priceNbxRoute)
-
-
-//Firi
-const spreadFiriRoute = require('./routes/firi/firi-spread')
-app.use('/firi/spread', cors(corsOptions), spreadFiriRoute)
-
-const priceFiriRoute = require('./routes/firi/firi-price')
-app.use('/firi/price', cors(corsOptions), priceFiriRoute)
-
-//Kaupang
-const spreadKaupangRoute = require('./routes/kaupang/kaupang-spread')
-app.use('/kaupang/spread', cors(corsOptions), spreadKaupangRoute)
-
-const priceKaupangRoute = require('./routes/kaupang/kaupang-price')
-app.use('/kaupang/price', cors(corsOptions), priceKaupangRoute)
-
-//Handlekrypto
-const spreadHandlekryptoRoute = require('./routes/handlekrypto/handlekrypto-spread')
-app.use('/handlekrypto/spread', cors(corsOptions), spreadHandlekryptoRoute)
-
-const priceHandlekryptoRoute = require('./routes/handlekrypto/handlekrypto-price')
-app.use('/handlekrypto/price', cors(corsOptions), priceHandlekryptoRoute)
-
-// ------- Bitmynt ------
-const spreadBitmyntRoute = require('./routes/bitmynt/bitmynt-spread')
-app.use('/bitmynt/spread', cors(corsOptions), spreadBitmyntRoute)
-
-const priceBitmyntRoute = require('./routes/bitmynt/bitmynt-price')
-app.use('/bitmynt/price', cors(corsOptions), priceBitmyntRoute)
 
 //Start message
 app.listen(
